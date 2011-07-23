d738aba481ce34de59defa1e7479951953a05fc0
Singlem <Singlem1134@gmail.com>
2011-07-23 19:52:46 +0800

    Some more packet parsing -- Empty packets -- Calendar Packets(still have more to do) -- Just making more true parsing

---
+++ src/WoWPacketViewer/Parsers/Other/EmptyPackets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WowTools.Core;
+
+namespace WowPacketParser.Parsing.Parsers
+{
+    public class EmptyPackets : Parser
+    {
+        [Parser(OpCodes.CMSG_CANCEL_TRADE)]
+        [Parser(OpCodes.CMSG_WORLD_STATE_UI_TIMER_UPDATE)]
+        [Parser(OpCodes.CMSG_CALENDAR_GET_CALENDAR)]
+        public void HandleEmptyPacket(Parser packet)
+        {
+            WriteLine("CMSG Packet that wants responce from server");
+        }
+    }
+}
+ 
