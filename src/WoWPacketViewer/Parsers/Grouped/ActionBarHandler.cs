9a992d0c1f1865094c6f2d1b08b042c3e331e05b
Singlem <Singlem1134@gmail.com>
2011-07-28 17:57:12 +0200

    Some more packet parsing

---
+++ src/WoWPacketViewer/Parsers/Grouped/ActionBarHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using WowTools.Core;
+
+namespace WoWPacketViewer
+{
+    public class ActionBarHandler : Parser
+    {
+        public enum ActionButtonType
+        {
+            Spell = 0,
+            Click = 1,
+            EquipSet = 32,
+            Macro = 64,
+            Item = 128
+        }
+
+        [Parser(OpCodes.SMSG_ACTION_BUTTONS)]
+        public void HandleInitialButtons(Parser packet)
+        {
+            packet.ReadByte("TalentSpec");
+
+            for (var i = 0; i < 144; i++)
+            {
+                var packed = packet.ReadInt32();
+
+                if (packed == 0)
+                    continue;
+                
+                var action = packed & 0x00FFFFFF;
+                Console.WriteLine("Action " + i + ": " + action);
+
+                var type = (ActionButtonType)((packed & 0xFF000000) >> 24);
+                Console.WriteLine("Type " + i + ": " + type);
+            }
+        }
+    }
+}
---
+++ src/WoWPacketViewer/Parsers/Grouped/CharacterHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using WowTools.Core;
+
+namespace WoWPacketViewer
+{
+    public class CharacterHandler : Parser
+    {
+        public enum Race
+        {
+            Human = 1,
+            Orc = 2,
+            Dwarf = 3,
+            NightElf = 4,
+            Scourge = 5,
+            Tauren = 6,
+            Gnome = 7,
+            Troll = 8,
+            Goblin = 9,
+            BloodElf = 10,
+            Draenei = 11,
+            FelOrc = 12,
+            Naga = 13,
+            Broken = 14,
+            Skeleton = 15,
+            Vrykul = 16,
+            Tuskarr = 17,
+            ForestTroll = 18,
+            Taunka = 19,
+            NorthrendSkeleton = 20,
+            IceTroll = 21
+        }
+
+        public enum Class
+        {
+            Warrior = 1,
+            Paladin = 2,
+            Hunter = 3,
+            Rogue = 4,
+            Priest = 5,
+            DeathKnight = 6,
+            Shaman = 7,
+            Mage = 8,
+            Warlock = 9,
+            Unknown = 10,
+            Druid = 11
+        }
+
+        public enum Gender
+        {
+            Male = 0,
+            Female = 1,
+            None = 2
+        }
+
+        [Parser(OpCodes.CMSG_CHAR_CREATE)]
+        public void HandleClientCharCreate(Parser packet)
+        {
+            packet.ReadString("Name");
+            packet.ReadEnum<Race>("Race");
+            packet.ReadEnum<Class>("Class");
+            packet.ReadEnum<Gender>("Gender");
+            packet.ReadByte("Face");
+            packet.ReadByte("HairStyle");
+            packet.ReadByte("HairColor");
+            packet.ReadByte("FacialHair");
+            packet.ReadByte("OutfitID");
+        }
+
+        [Parser(OpCodes.CMSG_CHAR_RENAME)]
+        public static void HandleClientCharRename(Parser packet)
+        {
+            packet.ReadInt64("GUID");
+            packet.ReadString("NewName");
+        }
+
+        [Parser(OpCodes.SMSG_CHAR_RENAME)]
+        public static void HandleServerCharRename(Parser packet)
+        {
+            packet.ReadByte("Response");
+            packet.ReadInt64("GUID");
+            packet.ReadString("Name");
+        }
+    }
+}
